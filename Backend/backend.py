e77c605630c668fda8e4181ed96ce18087adf403
MAYATTA NDIAYE <mndiaye27@etu.uqac.ca>
2024-11-27 09:47:34 -0500

    Page professionnel

--- Backend/backend.py
+++ Backend/backend.py
@@ -46,7 +46,7 @@ def inscription():
 
         if response.status_code == 201:
             flash("Inscription réussie !", "success")
-            return redirect(url_for("connexion"))  # Redirige vers la page de connexion après inscription
+            return redirect(url_for("professionnel"))  # Redirige vers la page de connexion après inscription
         else:
             flash("Erreur lors de l'inscription. Veuillez réessayer.", "danger")
 
@@ -77,11 +77,42 @@ def disponibilites(pageId):
 
     return render_template("disponibilites.html", disponibilites=disponibilites)
 
-@app.route("/page")
+@app.route("/page", methods=["GET", "POST"])
 def professionnel():
-    # Récupérer le prénom de l'utilisateur depuis la session
-    prenom = session.get('prenom', 'Utilisateur')  # Si pas de prénom, afficher 'Utilisateur'
-    return render_template("professionnel.html", prenom=prenom)  # Passer le prénom au template
+    if request.method == "POST":
+
+        page_name = request.form["page_name"]
+        description = request.form["description"]
+        visible = request.form["visible"] == 'true' 
+        activity = request.form["activity"]
+
+        response = requests.post(f"{API_URL}/page",json={
+            "Description" : description,
+            "page_name" : page_name,
+            "visible" : visible,
+            "activity" : activity
+
+        })
+
+    response = requests.get(f"{API_URL}/page")
+    if response.status_code == 200:
+        page_data = response.json()  # Récupère les données au format JSON
+        return render_template("professionnel.html",  page_name=page_data['page_name'],  description=page_data['description'], visible=page_data['visible'],activity=page_data['activity'])
+    else:
+                # Si la page n'existe pas ou une erreur se produit, on retourne une page d'erreur ou des données par défaut
+        return render_template("professionnel.html",  page_name="Erreur",  description="Page introuvable", visible=True, activity="chomeur")
+       
+@app.route("/page/<int:pageId>", methods=["GET", "POST"])
+def pro():
+   
+    response = requests.get(f"{API_URL}/page/<int:pageId>")
+    if response.status_code == 200:
+        page_data = response.json()  # Récupère les données au format JSON
+        return render_template("professionnel.html",  page_name=page_data['page_name'],  description=page_data['description'], visible=page_data['visible'],activity=page_data['activity'])
+    else:
+                # Si la page n'existe pas ou une erreur se produit, on retourne une page d'erreur ou des données par défaut
+        return render_template("professionnel.html",  page_name="Erreur",  description="Page introuvable", visible=True, activity="chomeur")
+         
 
 
 if __name__ == "__main__":
